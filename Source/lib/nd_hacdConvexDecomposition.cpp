2aaa27d9f0625b9fa7055f9a7f3803e8d4edacba
Nicky <sl.nicky.ml@googlemail.com>
2011-09-02 09:48:05 +0200

    Consitant formatting using Artistic Style -t -b -S -N -w -D -p

--- Source/lib/nd_hacdConvexDecomposition.cpp
+++ Source/lib/nd_hacdConvexDecomposition.cpp
@@ -1,16 +1,16 @@
 /**
  * copyright 2011 sl.nicky.ml@googlemail.com
- * 
+ *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation;
  * version 2.1 of the License only.
- * 
+ *
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
@@ -62,37 +62,37 @@ LLCDResult nd_hacdConvexDecomposition::quitSystem()
 	return LLCD_OK;
 }
 
-void nd_hacdConvexDecomposition::genDecomposition(int& decomp)
+void nd_hacdConvexDecomposition::genDecomposition( int& decomp )
 {
-  HACDDecoder *pGen = new HACDDecoder();
-  decomp = mNextId;
-  ++mNextId;
-  
-  mDecoders[ decomp ] = pGen;
+	HACDDecoder *pGen = new HACDDecoder();
+	decomp = mNextId;
+	++mNextId;
+
+	mDecoders[ decomp ] = pGen;
 }
 
-void nd_hacdConvexDecomposition::deleteDecomposition(int decomp)
+void nd_hacdConvexDecomposition::deleteDecomposition( int decomp )
 {
-  delete mDecoders[ decomp ];
-  mDecoders.erase( decomp );
+	delete mDecoders[ decomp ];
+	mDecoders.erase( decomp );
 }
 
-void nd_hacdConvexDecomposition::bindDecomposition(int decomp)
+void nd_hacdConvexDecomposition::bindDecomposition( int decomp )
 {
-  mCurrentDecoder = decomp;
+	mCurrentDecoder = decomp;
 }
 
-LLCDResult nd_hacdConvexDecomposition::setParam(const char* name, float val)
-{
-	return LLCD_NOT_IMPLEMENTED;
-}
-
-LLCDResult nd_hacdConvexDecomposition::setParam(const char* name, bool val)
+LLCDResult nd_hacdConvexDecomposition::setParam( const char* name, float val )
 {
 	return LLCD_NOT_IMPLEMENTED;
 }
 
-LLCDResult nd_hacdConvexDecomposition::setParam(const char* name, int val)
+LLCDResult nd_hacdConvexDecomposition::setParam( const char* name, bool val )
+{
+	return LLCD_NOT_IMPLEMENTED;
+}
+
+LLCDResult nd_hacdConvexDecomposition::setParam( const char* name, int val )
 {
 	return LLCD_NOT_IMPLEMENTED;
 }
@@ -103,7 +103,7 @@ LLCDResult nd_hacdConvexDecomposition::setMeshData( const LLCDMeshData* data, bo
 	return ::setMeshData( data, vertex_based, pC );
 }
 
-LLCDResult nd_hacdConvexDecomposition::registerCallback(int stage, llcdCallbackFunc callback )
+LLCDResult nd_hacdConvexDecomposition::registerCallback( int stage, llcdCallbackFunc callback )
 {
 	return LLCD_NOT_IMPLEMENTED;
 }
@@ -113,9 +113,9 @@ LLCDResult nd_hacdConvexDecomposition::buildSingleHull()
 	return LLCD_OK;
 }
 
-LLCDResult nd_hacdConvexDecomposition::executeStage(int stage)
+LLCDResult nd_hacdConvexDecomposition::executeStage( int stage )
 {
-	if( stage <0 || stage >= NUM_STAGES )
+	if ( stage < 0 || stage >= NUM_STAGES )
 		return LLCD_INVALID_STAGE;
 
 	HACDDecoder *pC = mDecoders[ mCurrentDecoder ];
@@ -126,18 +126,18 @@ LLCDResult nd_hacdConvexDecomposition::executeStage(int stage)
 	delete pHACD;
 
 	pC->mStages[ stage ] = oRes;
-	
+
 	return LLCD_OK;
 }
 
-int nd_hacdConvexDecomposition::getNumHullsFromStage(int stage)
+int nd_hacdConvexDecomposition::getNumHullsFromStage( int stage )
 {
 	HACDDecoder *pC = mDecoders[ mCurrentDecoder ];
 
-	if( !pC )
+	if ( !pC )
 		return 0;
 
-	if( stage < 0 || stage >= pC->mStages.size() )
+	if ( stage < 0 || stage >= pC->mStages.size() )
 		return 0;
 
 	return pC->mStages[stage].mHulls.size();
@@ -145,39 +145,39 @@ int nd_hacdConvexDecomposition::getNumHullsFromStage(int stage)
 
 DecompData toSingleHull( HACDDecoder *aDecoder, LLCDResult &aRes )
 {
-  aRes = LLCD_REQUEST_OUT_OF_RANGE;
+	aRes = LLCD_REQUEST_OUT_OF_RANGE;
 
-  for( int i = 0; i < TO_SINGLE_HULL_TRIES; ++i )
-    {
-      tHACD *pHACD = init( CONCAVITY_FOR_SINGLE_HULL[i], 1, MAX_VERTICES_PER_HULL, CONNECT_DISTS[i], aDecoder );
-
-      DecompData oRes = decompose( pHACD );
-      delete pHACD;
-
-      if( oRes.mHulls.size() == 1 )
+	for ( int i = 0; i < TO_SINGLE_HULL_TRIES; ++i )
 	{
-	  aRes = LLCD_OK;
-	  return oRes;
-	}
-    }
+		tHACD *pHACD = init( CONCAVITY_FOR_SINGLE_HULL[i], 1, MAX_VERTICES_PER_HULL, CONNECT_DISTS[i], aDecoder );
 
-  return DecompData();
+		DecompData oRes = decompose( pHACD );
+		delete pHACD;
+
+		if ( oRes.mHulls.size() == 1 )
+		{
+			aRes = LLCD_OK;
+			return oRes;
+		}
+	}
+
+	return DecompData();
 }
 
-LLCDResult nd_hacdConvexDecomposition::getSingleHull( LLCDHull* hullOut ) 
+LLCDResult nd_hacdConvexDecomposition::getSingleHull( LLCDHull* hullOut )
 {
-  HACDDecoder *pC = mDecoders[ mCurrentDecoder ];
+	HACDDecoder *pC = mDecoders[ mCurrentDecoder ];
 
-  LLCDResult res;
-  DecompData oRes = ::toSingleHull( pC, res );
-  
-  if( LLCD_OK != res || oRes.mHulls.size() != 1 )
-    return res;
+	LLCDResult res;
+	DecompData oRes = ::toSingleHull( pC, res );
 
-  pC->mSingleHull = oRes.mHulls[0];
-  pC->mSingleHull.toLLHull( hullOut );
+	if ( LLCD_OK != res || oRes.mHulls.size() != 1 )
+		return res;
 
-  return LLCD_OK;
+	pC->mSingleHull = oRes.mHulls[0];
+	pC->mSingleHull.toLLHull( hullOut );
+
+	return LLCD_OK;
 }
 
 LLCDResult nd_hacdConvexDecomposition::getHullFromStage( int stage, int hull, LLCDHull* hullOut )
@@ -186,14 +186,14 @@ LLCDResult nd_hacdConvexDecomposition::getHullFromStage( int stage, int hull, LL
 
 	memset( hullOut, 0, sizeof( LLCDHull ) );
 
-	if( stage < 0 || stage >= pC->mStages.size() )
+	if ( stage < 0 || stage >= pC->mStages.size() )
 		return LLCD_INVALID_STAGE;
-  
+
 	DecompData &oData = pC->mStages[ stage ];
 
-	if( hull < 0 || hull >= oData.mHulls.size() )
+	if ( hull < 0 || hull >= oData.mHulls.size() )
 		return LLCD_REQUEST_OUT_OF_RANGE;
-	
+
 	oData.mHulls[ hull ].toLLHull( hullOut );
 	return LLCD_OK;
 }
@@ -204,13 +204,13 @@ LLCDResult nd_hacdConvexDecomposition::getMeshFromStage( int stage, int hull, LL
 
 	memset( meshDataOut, 0, sizeof( LLCDHull ) );
 
-	if( stage < 0 || stage >= pC->mStages.size() )
-	  return LLCD_INVALID_STAGE;
-  
+	if ( stage < 0 || stage >= pC->mStages.size() )
+		return LLCD_INVALID_STAGE;
+
 	DecompData &oData = pC->mStages[ stage ];
 
-	if( hull < 0 || hull >= oData.mHulls.size() )
-	  return LLCD_REQUEST_OUT_OF_RANGE;
+	if ( hull < 0 || hull >= oData.mHulls.size() )
+		return LLCD_REQUEST_OUT_OF_RANGE;
 
 	oData.mHulls[ hull ].toLLMesh( meshDataOut );
 	return LLCD_OK;
@@ -218,41 +218,41 @@ LLCDResult nd_hacdConvexDecomposition::getMeshFromStage( int stage, int hull, LL
 
 LLCDResult	nd_hacdConvexDecomposition::getMeshFromHull( LLCDHull* hullIn, LLCDMeshData* meshOut )
 {
-	memset( meshOut, 0, sizeof(LLCDMeshData) );
+	memset( meshOut, 0, sizeof( LLCDMeshData ) );
 	return LLCD_NOT_IMPLEMENTED;
 }
 
-LLCDResult nd_hacdConvexDecomposition::generateSingleHullMeshFromMesh(LLCDMeshData* meshIn, LLCDMeshData* meshOut)
+LLCDResult nd_hacdConvexDecomposition::generateSingleHullMeshFromMesh( LLCDMeshData* meshIn, LLCDMeshData* meshOut )
 {
 	memset( meshOut, 0, sizeof( LLCDMeshData ) );
 	mSingleHullMeshFromMesh->clear();
-	LLCDResult res = ::setMeshData( meshIn, meshIn->mNumVertices>3, mSingleHullMeshFromMesh );
+	LLCDResult res = ::setMeshData( meshIn, meshIn->mNumVertices > 3, mSingleHullMeshFromMesh );
 
-	if( LLCD_OK != res )
-	  return res;
+	if ( LLCD_OK != res )
+		return res;
 
 	DecompData oRes = ::toSingleHull( mSingleHullMeshFromMesh, res );
-  
-	if( LLCD_OK != res || oRes.mHulls.size() != 1 )
-	  return res;
+
+	if ( LLCD_OK != res || oRes.mHulls.size() != 1 )
+		return res;
 
 	mSingleHullMeshFromMesh->mSingleHull = oRes.mHulls[0];
 	mSingleHullMeshFromMesh->mSingleHull.toLLMesh( meshOut );
- 
+
 	return LLCD_OK;
 }
 
-void nd_hacdConvexDecomposition::loadMeshData(const char* fileIn, LLCDMeshData** meshDataOut)
+void nd_hacdConvexDecomposition::loadMeshData( const char* fileIn, LLCDMeshData** meshDataOut )
 {
 	static LLCDMeshData meshData;
-	memset( &meshData, 0, sizeof(LLCDMeshData) );
+	memset( &meshData, 0, sizeof( LLCDMeshData ) );
 	*meshDataOut = &meshData;
 }
 
 int nd_hacdConvexDecomposition::getParameters( const LLCDParam** paramsOut )
 {
 	static LLCDParam oParams[1];
-	memset( &oParams[0], 0, sizeof(LLCDParam) );
+	memset( &oParams[0], 0, sizeof( LLCDParam ) );
 	oParams[0].mName = "nd_AlwaysNeedTriangles";
 	oParams[0].mDescription = 0;
 	oParams[0].mType = LLCDParam::LLCD_BOOLEAN;
@@ -264,11 +264,11 @@ int nd_hacdConvexDecomposition::getParameters( const LLCDParam** paramsOut )
 
 int nd_hacdConvexDecomposition::getStages( const LLCDStageData** stagesOut )
 {
-  static LLCDStageData oStages[2];
+	static LLCDStageData oStages[2];
 
-  oStages[0].mName = "Decompose";
-  oStages[0].mDescription = NULL;
+	oStages[0].mName = "Decompose";
+	oStages[0].mDescription = NULL;
 
-  *stagesOut = oStages;
-  return 1;
+	*stagesOut = oStages;
+	return 1;
 }
--- Source/lib/nd_hacdUtils.cpp
+++ Source/lib/nd_hacdUtils.cpp
@@ -1,16 +1,16 @@
 /**
  * copyright 2011 sl.nicky.ml@googlemail.com
- * 
+ *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation;
  * version 2.1 of the License only.
- * 
+ *
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
@@ -23,47 +23,47 @@ tHACD* init( int nConcavity, int nClusters, int nMaxVerticesPerHull, double dMax
 	tHACD *pDec = new tHACD();
 	pDec->SetPoints( &aData->mVertices[0] );
 	pDec->SetNPoints( aData->mVertices.size() );
-  
-	if( aData->mTriangles.size() )
-    {
+
+	if ( aData->mTriangles.size() )
+	{
 		pDec->SetTriangles( &aData->mTriangles[0] );
 		pDec->SetNTriangles( aData->mTriangles.size() );
-    }
-  
-	pDec->SetCompacityWeight(0.1f);
-	pDec->SetVolumeWeight(0);
+	}
+
+	pDec->SetCompacityWeight( 0.1f );
+	pDec->SetVolumeWeight( 0 );
 	pDec->SetNClusters( nClusters );
-	pDec->SetAddExtraDistPoints(false);
-	pDec->SetAddNeighboursDistPoints(false);
-	pDec->SetAddFacesPoints(true);
+	pDec->SetAddExtraDistPoints( false );
+	pDec->SetAddNeighboursDistPoints( false );
+	pDec->SetAddFacesPoints( true );
 	pDec->SetNVerticesPerCH( nMaxVerticesPerHull );
 	pDec->SetConcavity( nConcavity );
 	pDec->SetConnectDist( dMaxConnectDist );
-  
+
 	return pDec;
 }
 
 DecompData decompose( tHACD *aHACD )
 {
 	aHACD->Compute();
-	
+
 	DecompData oRet;
 
 	int nClusters = aHACD->GetNClusters();
-	
-	for( int i = 0; i < nClusters; ++i )
-    {
-		int nVertices = aHACD->GetNPointsCH(0);
-		int nTriangles = aHACD->GetNTrianglesCH(0);
+
+	for ( int i = 0; i < nClusters; ++i )
+	{
+		int nVertices = aHACD->GetNPointsCH( 0 );
+		int nTriangles = aHACD->GetNTrianglesCH( 0 );
 		DecompHull oHull;
 
 		oHull.mVertices.resize( nVertices );
 		oHull.mTriangles.resize( nTriangles );
 		aHACD->GetCH( 0, &oHull.mVertices[0], &oHull.mTriangles[0] );
-      
+
 		oRet.mHulls.push_back( oHull );
 
-    }
+	}
 
 	return oRet;
 }
@@ -88,42 +88,42 @@ tVecLong fromI32( void *& pPtr, int aStride )
 
 LLCDResult setMeshData( const LLCDMeshData* data, bool vertex_based, HACDDecoder *aDec )
 {
-	if ( !data || !data->mVertexBase || (data->mNumVertices < 3) || (data->mVertexStrideBytes != 12 && data->mVertexStrideBytes != 16) )
+	if ( !data || !data->mVertexBase || ( data->mNumVertices < 3 ) || ( data->mVertexStrideBytes != 12 && data->mVertexStrideBytes != 16 ) )
 		return LLCD_INVALID_MESH_DATA;
 
-	if( !vertex_based && ( (data->mNumTriangles < 1) || ! data->mIndexBase ) )
+	if ( !vertex_based && ( ( data->mNumTriangles < 1 ) || ! data->mIndexBase ) )
 		return LLCD_INVALID_MESH_DATA;
 
 	aDec->clear();
 	int nCount = data->mNumVertices;
-	float* pVertex = const_cast<float*>(data->mVertexBase);
-	int nStride = data->mVertexStrideBytes / sizeof(float);
+	float* pVertex = const_cast<float*>( data->mVertexBase );
+	int nStride = data->mVertexStrideBytes / sizeof( float );
 
-	for( int i = 0; i < nCount; ++i )
+	for ( int i = 0; i < nCount; ++i )
 	{
 		tVecDbl oPt( pVertex[0], pVertex[1], pVertex[2] );
 		aDec->mVertices.push_back( oPt );
 		pVertex += nStride;
-    }
+	}
 
-	if( !vertex_based )
-    {
+	if ( !vertex_based )
+	{
 		fFromIXX pFunc = 0;
 		nCount = data->mNumTriangles;
 		nStride = data->mIndexStrideBytes;
-		void *pData = const_cast<void*>(data->mIndexBase);
-      
+		void *pData = const_cast<void*>( data->mIndexBase );
+
 		if ( data->mIndexType == LLCDMeshData::INT_16 )
 			pFunc = fromI16;
 		else
 			pFunc = fromI32;
 
-		for( int i = 0; i < nCount; ++i )
+		for ( int i = 0; i < nCount; ++i )
 		{
-			tVecLong oVal( (*pFunc)( pData, nStride ) );
+			tVecLong oVal( ( *pFunc )( pData, nStride ) );
 			aDec->mTriangles.push_back( oVal );
 		}
-    }
+	}
 
 	return LLCD_OK;
 }
